b96c5f554fe7bfca532509b4f61817ab883178b9
behrouz43salimi <143632590+stinetheslayer@users.noreply.github.com>
2025-07-29 11:08:53 +0200

    refactor(Header): import logo image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import logo from '../assets/investment-calculator-logo.png'
+
 export default function Header() {
     return (
         <header id="header">
-            <img src="/investment-calculator-logo.png" alt="investments logo" />
+            <img src={logo} alt="Logo showing a money bag" />
             <h1>Investment Calculator</h1>
         </header>
     )
