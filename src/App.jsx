15ea36aa55a24f9f580a66c1732598d5ec828096
behrouz43salimi <143632590+stinetheslayer@users.noreply.github.com>
2025-07-29 10:44:04 +0200

    feat(Results): add Results component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // import './investment-calculator-logo.png'
 import Header from "./components/Header.jsx"
 import UserInput from "./components/UserInput.jsx"
+import Results from "./components/Results.jsx"
 
 function App() {
   return (
       <>
     <Header />
     <UserInput />
+    <Results />
       </>
 
   )
---
+++ src/components/Results.jsx
@@ -0,0 +1,26 @@
+export default function Results() {
+    return (
+        <div>
+            <table id="result">
+                <thead>
+                    <tr>
+                        <th>Year</th>
+                        <th>Investment Value</th>
+                        <th>Interest (Year)</th>
+                        <th>Total Interest</th>
+                        <th>Invested Capital</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Data 2</td>
+                        <td>Data 3</td>
+                        <td>Data 4</td>
+                        <td>Data 5</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
