a8d545bc8ac4b6b843747bbb855e5ea8724a2dad
behrouz43salimi <143632590+stinetheslayer@users.noreply.github.com>
2025-07-29 14:24:09 +0200

    fix: show message for invalid duration

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,8 @@ function App() {
         duration: 10
     });
 
+    const inputIsValid = userInput.duration >= 1;
+
     function handleChange(inputIdentifier, newValue) {
         setUserInput(prevUserInput => {
             return {
@@ -28,7 +30,10 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange}/>
-      <Results input={userInput} />
+        {!inputIsValid && (
+            <p className="center">Please enter a duration greater than zero.</p>
+        )}
+        {inputIsValid && <Results input={userInput} />}
     </>
   )
 }
