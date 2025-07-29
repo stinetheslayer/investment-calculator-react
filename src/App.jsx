ad57ce66b2e2a984f1a021f154637cdd7cd8628c
behrouz43salimi <143632590+stinetheslayer@users.noreply.github.com>
2025-07-29 09:46:42 +0200

    feat(Header): add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+// import './investment-calculator-logo.png'
+import Header from "./components/Header.jsx"
+
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
+      <>
+    <Header />
+      </>
+
   )
 }
 
